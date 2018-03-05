2d5b6f916a56a1b4037c1172cee9c54221d75464
DomNelson <nelson.dominic@gmail.com>
2018-03-05 16:09:07 -0500

    Extend CLI

--- scripts/tmrca.py
+++ scripts/tmrca.py
@@ -5,12 +5,21 @@ import gzip
 
 import ete3
 
-jarfile = os.path.expanduser(sys.argv[1])
-outpath = os.path.expanduser(sys.argv[2])
+try:
+    jarfile = os.path.expanduser(sys.argv[1])
+    outpath = os.path.expanduser(sys.argv[2])
+    N = int(sys.argv[3])
+    sample_size = int(sys.argv[4])
+    base_pairs = float(sys.argv[5])
+except:
+    print('''
+    Usage: python tmrca.py PATH_TO_ARGON_JARFILE OUTPATH N_E SAMPLE_SIZE LEN_IN_BPS
+    ''')
+    sys.exit()
+
+size = base_pairs / 1e6
 tmrca_file = os.path.join(outpath, 'tmrca.txt')
 
-N = 1000
-sample_size = 100
 rho = 1e-8
 mu = 1e-7
 
@@ -27,6 +36,7 @@ command += ' -N ' + str(N)
 command += ' -pop 1 ' + str(sample_size)
 command += ' -rec ' + str(rho)
 command += ' -mut ' + str(mu)
+command += ' -size ' + str(size)
 command += ' -trees -age'
 command += ' -out ' + prefix
 
