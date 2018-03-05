f522d91672bd94c5e981abce64d1cd72ad789d09
Dominic Nelson <dnelson@gra-login2.graham.sharcnet>
2018-03-05 17:54:20 -0500

    Cleanup ARGON output files

--- scripts/tmrca.py
+++ scripts/tmrca.py
@@ -11,17 +11,18 @@ try:
     N = int(sys.argv[3])
     sample_size = int(sys.argv[4])
     base_pairs = float(sys.argv[5])
+    n_iter = int(sys.argv[6])
 except:
     print('''
-    Usage: python tmrca.py PATH_TO_ARGON_JARFILE OUTPATH N_E SAMPLE_SIZE LEN_IN_BPS
+    Usage:
+    python tmrca.py PATH_TO_ARGON_JARFILE OUTPATH N_E SAMPLE_SIZE LEN_IN_BPS N_ITER
     ''')
     sys.exit()
 
 size = base_pairs / 1e6
-tmrca_file = os.path.join(outpath, 'tmrca.txt')
 
 rho = 1e-8
-mu = 1e-7
+mu = 1e-8
 
 assert N > sample_size
 
@@ -29,7 +30,9 @@ prefix = outpath + '/'
 prefix += 'N' + str(N)
 prefix += 'rec' + str(rho)
 prefix += 'mut' + str(mu)
+prefix += 'length' + str(base_pairs)
 treefile = prefix + '.trees'
+tmrca_file = prefix + '_tmrca.txt'
 
 command = 'java -jar ' + jarfile
 command += ' -N ' + str(N)
@@ -40,13 +43,17 @@ command += ' -size ' + str(size)
 command += ' -trees -age'
 command += ' -out ' + prefix
 
-ret = subprocess.run(command, shell=True, check=True)
+for i in range(n_iter):
+    ret = subprocess.run(command, shell=True, check=True)
 
-with open(treefile, 'r') as f:
-    with open(tmrca_file, 'w') as g:
-        for line in f:
-            tree = ete3.Tree(line.split()[3], format=1)
-            root = tree.get_tree_root()
-            tmrca = root.name.split('_')[0]
+    with open(treefile, 'r') as f:
+        with open(tmrca_file, 'a') as g:
+            for line in f:
+                tree = ete3.Tree(line.split()[3], format=1)
+                root = tree.get_tree_root()
+                tmrca = root.name.split('_')[0]
 
-            g.write(tmrca + '\n')
+                g.write(tmrca + '\n')
+
+cleanup = 'rm ' + prefix + '.*'
+subprocess.run(cleanup, shell=True, check=True)
