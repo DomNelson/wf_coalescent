b92710cd9652680a04771eb49ef118e7296417c9
Dominic Nelson <nelson.dominic@gmail.com>
2019-02-22 16:54:34 -0500

    Add tracts variance decay to plot

--- scripts/admixture_props.py
+++ scripts/admixture_props.py
@@ -13,6 +13,22 @@ import pandas as pd
 from tqdm import tqdm
 
 
+def gravel_ancestry_variance(T, m, L, K, N):
+    """
+    T - admixture time
+    m - migration rate
+    L - total length
+    K - number of chromosomes
+    N - effective population size
+    """
+    A = m * (1 - m) / 2 ** (T - 1)
+    B_num = 2 * m * (1 - m) * (1 - 1 / (2 * N)) ** (T - 1)
+    B_denom = 2 * K + 2 * (T - 2) * L
+    B = B_num / B_denom
+    
+    return A + B
+
+
 def get_positions_rates(chrom_lengths, rho):
     """
     Takes a list of chromosome lengths and returns lists of positions and
@@ -56,6 +72,8 @@ def get_ind_tracts(ts, max_time):
             for s in samples:
                 ind_tracts[s].append(length)
             stored = True
+
+    # import IPython; IPython.embed()
                 
     return ind_tracts
 
@@ -65,6 +83,8 @@ def get_ancestry_props(replicates, max_time, num_replicates):
 
     with tqdm(total=num_replicates, desc=str(max_time)) as pbar:
         for ts in replicates:
+            # from IPython import embed; embed()
+            # sys.exit()
             ind_tracts = get_ind_tracts(ts, max_time)
             total_length = ts.get_sequence_length()
 
@@ -72,6 +92,7 @@ def get_ancestry_props(replicates, max_time, num_replicates):
             samples = iter(ts.samples())
             for sample in samples:
                 sample_copy = next(samples)
+                ## Convert to Morgans
                 prop = sum(ind_tracts[sample]) / total_length
                 replicate_props.append(prop)
 
@@ -81,18 +102,7 @@ def get_ancestry_props(replicates, max_time, num_replicates):
     return ancestry_props
 
 
-def simulate(args, admixture_time):
-    rho = 1e-8
-    all_lengths = [247249719, 242951149, 199501827, 191273063, 180857866,
-            170899992, 158821424, 146274826, 140273252, 135374737, 134452384,
-            132349534, 114142980, 106368585, 100338915, 88827254, 78774742,
-            76117153, 63811651, 62435964, 46944323, 49691432]
-    chrom_lengths = all_lengths[:args.num_chroms]
-
-    positions, rates = get_positions_rates(chrom_lengths, rho)
-    num_loci = positions[-1]
-    recombination_map = msprime.RecombinationMap(
-            positions, rates, num_loci=num_loci)
+def simulate(args, recombination_map, admixture_time):
 
     population_configurations = [
             msprime.PopulationConfiguration(
@@ -105,6 +115,12 @@ def simulate(args, admixture_time):
             )
     ]
 
+    migration_matrix = None
+    # [
+    #     [0, args.admixture_prop],
+    #     [0,       0],
+    # ]
+
     demographic_events = [
         msprime.MassMigration(
             time=admixture_time,
@@ -112,6 +128,7 @@ def simulate(args, admixture_time):
             dest=1,
             proportion=args.admixture_prop
         ),
+        msprime.MigrationRateChange(time=admixture_time, rate=0),
         msprime.MassMigration(
             time=admixture_time + 1,
             source=1,
@@ -128,6 +145,7 @@ def simulate(args, admixture_time):
     replicates = msprime.simulate(
             recombination_map=recombination_map,
             demographic_events=demographic_events,
+            migration_matrix=migration_matrix,
             population_configurations=population_configurations,
             model=args.model,
             record_migrations=True,
@@ -149,27 +167,54 @@ def get_output_suffix(args, admixture_time):
 
 
 def main(args):
+
     if args.paper_params:
+        print("*" * 79)
         print("Reproducing figure from paper - ignoring all other args" +\
                 " except --model and --out_dir")
-        admixture_times = [x for x in range(1, 20)]
-        admixture_times += [x for x in range(20, 50, 2)]
-        admixture_times += [x for x in range(50, 100, 5)]
-        admixture_times += [x for x in range(100, 200, 10)]
-        admixture_times += [x for x in range(200, 500, 25)]
+        print("*" * 79)
+        admixture_times = [x for x in [1, 2]]
+        # admixture_times = [x for x in range(1, 20)]
+        # admixture_times += [x for x in range(20, 50, 4)]
+        # admixture_times += [x for x in range(50, 100, 10)]
+        # admixture_times += [x for x in range(100, 200, 10)]
+        # admixture_times += [x for x in range(200, 500, 25)]
 
-        paper_args = argparse.Namespace(
-                Ne=1000,
-                sample_size=80,
+        args = argparse.Namespace(
+                Ne=100,
+                sample_size=100,
                 model=args.model,
                 admixture_prop=0.3,
                 num_chroms=22,
-                replicates=1,
-                out_dir=args.out_dir
+                replicates=50,
+                out_dir=args.out_dir,
+                discretize_hack=True,
+                plot=True,
                 )
     else:
         admixture_range = [int(x.strip()) for x in args.admixture_range.split(',')]
         admixture_times = range(*admixture_range)
+
+
+    rho = 1e-8
+    all_lengths = [247249719, 242951149, 199501827, 191273063, 180857866,
+            170899992, 158821424, 146274826, 140273252, 135374737, 134452384,
+            132349534, 114142980, 106368585, 100338915, 88827254, 78774742,
+            76117153, 63811651, 62435964, 46944323, 49691432]
+    chrom_lengths = all_lengths[:args.num_chroms]
+
+    positions, rates = get_positions_rates(chrom_lengths, rho)
+    num_loci = positions[-1]
+
+    ## HACK: This is to avoid bad edge intervals (right <= left) when
+    ## simulating whole genomes. Possible overflow / floating point precision
+    ## issue
+    if args.discretize_hack:
+        num_loci = int(num_loci / 100)
+        print("Scaling to", num_loci, "loci")
+
+    recombination_map = msprime.RecombinationMap(
+            positions, rates, num_loci=num_loci)
     
     nrows = len(admixture_times)
     ncols = args.replicates
@@ -178,7 +223,7 @@ def main(args):
 
     df = pd.DataFrame()
     for i, t in enumerate(admixture_times):
-        replicates = simulate(args, admixture_time=t)
+        replicates = simulate(args, recombination_map, admixture_time=t)
         props_replicates = get_ancestry_props(
                 replicates,
                 max_time=t,
@@ -197,11 +242,28 @@ def main(args):
     suffix = get_output_suffix(args, admixture_time=t)
     average_variance.to_csv(os.path.join(basedir, suffix + '.txt'))
 
-    # fig, ax = plt.subplots()
-    # average_variance.plot(ax=ax, logx=True, logy=True)
-    # plot_file = os.path.join(basedir, suffix + '.png')
-    # fig.savefig(plot_file)
-    # import IPython; IPython.embed()
+    if args.plot:
+
+        length_in_morgans = positions[-1] / 1e8
+        gravel_variance = [
+                gravel_ancestry_variance(
+                        T,
+                        args.admixture_prop,
+                        length_in_morgans,
+                        args.num_chroms,
+                        args.Ne
+                ) for T in admixture_times]
+        print("Comparing vs tracts with", length_in_morgans, "Morgans")
+
+        plot_file = os.path.join(basedir, suffix + '.png')
+
+        fig, ax = plt.subplots()
+        ax.plot(admixture_times, gravel_variance, label='Expected')
+        average_variance.plot(ax=ax, label=str(args.model))
+        ax.set_xscale('log')
+        ax.set_yscale('log')
+        fig.legend()
+        fig.savefig(plot_file)
 
 
 if __name__ == "__main__":
@@ -214,6 +276,8 @@ if __name__ == "__main__":
     parser.add_argument('--admixture_prop', type=float, default=0.3)
     parser.add_argument('--num_chroms', type=int, default=22)
     parser.add_argument('--replicates', type=int, default=1)
+    parser.add_argument('--plot', action='store_true')
+    parser.add_argument('--discretize_hack', action='store_true')
     parser.add_argument('--out_dir', default=os.path.expanduser('~/temp/'))
 
     args = parser.parse_args()
