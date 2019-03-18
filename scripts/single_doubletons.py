cd42c744db172fa32f0bd143e3df49d0ad7b9654
DomNelson <nelson.dominic@gmail.com>
2019-03-18 14:29:32 -0400

    Change to long-form dataframe

--- scripts/single_doubletons.py
+++ scripts/single_doubletons.py
@@ -6,6 +6,7 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 from collections import Counter
 from tqdm import tqdm
@@ -17,14 +18,14 @@ def get_positions_rates(num_chroms=22, rho=1e-8):
     rates to pass to msprime.RecombinationMap
     """
     ## TODO: Check these lengths!
-    # chrom_lengths: typing.List[float] = [1e8]
-    chrom_lengths: typing.List[float] = [
-            277693825, 263349606, 224483368, 212778391,
-            203765845, 192951739, 186795932, 170176519,
-            168073935, 178947388, 159485425, 172777271,
-            126940475, 116331251, 12554709, 13489110,
-            12929210, 11897848, 10779606, 10792434,
-            61526812, 72706815]
+    chrom_lengths: typing.List[float] = [1e6]
+    # chrom_lengths: typing.List[float] = [
+    #         277693825, 263349606, 224483368, 212778391,
+    #         203765845, 192951739, 186795932, 170176519,
+    #         168073935, 178947388, 159485425, 172777271,
+    #         126940475, 116331251, 12554709, 13489110,
+    #         12929210, 11897848, 10779606, 10792434,
+    #         61526812, 72706815]
 
     positions = []
     rates = []
@@ -52,13 +53,36 @@ def get_recombination_map(num_chroms, rho=1e-8):
     return recombination_map
 
 
+def run_hybrid_simulations(sim_kwargs, hybrid_wf_gens):
+    """
+    TODO: Use this for dtwf sims
+    """
+    hybrid_kwargs = sim_kwargs.copy()
+    hybrid_kwargs['model'] = 'dtwf'
+    dtwf_ts = msprime.simulate(
+                __tmp_max_time=hybrid_wf_gens,
+                **hybrid_kwargs,
+                )
+
+    hybrid_kwargs['model'] = 'hudson'
+    Ne = hybrid_kwargs['population_configurations'][0].initial_size
+    hybrid_kwargs['population_configurations'] = None
+    hybrid_ts = msprime.simulate(
+            from_ts=dtwf_ts,
+            Ne=Ne,
+            **hybrid_kwargs,
+            )
+
+    return hybrid_ts
+
+
 def simulate(model=None, num_replicates=1, num_chroms=22, rho=1e-8):
     if model is None:
         model = 'hudson'
 
     pc = msprime.PopulationConfiguration(
             initial_size=10000,
-            sample_size=10000,
+            sample_size=20000,
             growth_rate=0
             )
 
@@ -78,7 +102,7 @@ def simulate(model=None, num_replicates=1, num_chroms=22, rho=1e-8):
 
 def plot_singletons_doubletons(sfs_df, outfile=None):
     if outfile is None:
-        outfile = os.path.expanduser('~/temp/test_singletons_doubletons_plot.png')
+        outfile = os.path.expanduser('~/temp/test2_singletons_doubletons_plot.png')
 
     max_count = 2
 
@@ -118,7 +142,7 @@ def plot_singletons_doubletons(sfs_df, outfile=None):
 
 def deprecated_plot_sfs(sfs_dict, models, max_count=None, outfile=None):
     if outfile is None:
-        outfile = os.path.expanduser('~/temp/test_sfs_plot.png')
+        outfile = os.path.expanduser('~/temp/test2_sfs_plot.png')
 
     fig, ax = plt.subplots()
 
@@ -147,7 +171,7 @@ def deprecated_plot_sfs(sfs_dict, models, max_count=None, outfile=None):
     fig.savefig(outfile)
 
 
-def get_sfs(ts):
+def get_sfs(ts, model):
     counts = []
 
     for tree in ts.trees():
@@ -161,12 +185,29 @@ def get_sfs(ts):
     sfs = [sfs_counter[n] for n in range(1, max(sfs_counter.keys()) + 1)]
     print("Singletons:", sfs[0], "Doubletons:", sfs[1])
 
-    return sfs
+    df = pd.DataFrame()
+    df['frequency'] = range(1, max(sfs_counter.keys()) + 1)
+    df['count'] = sfs
+    df['model'] = model
+
+    return df
+
+
+def plot_sfs_violin(sfs_df=None, sfs_file=None, plot_outfile=None, max_freq=2):
+    if sfs_df is None:
+        assert sfs_file is not None
+        sfs_df = pd.read_csv(os.path.expanduser(sfs_file), index_col=0)
+
+    if plot_outfile is None:
+        plot_outfile = os.path.expanduser('~/temp/test_violin.pdf')
+
+    samp = df[df['frequency'] <= max_freq]
+    sns.violinplot(x='frequency', y='count', hue='model', data=samp)
 
 
 def write_sfs(sfs_df, outfile=None):
     if outfile is None:
-        outfile = os.path.expanduser('~/temp/test_write_sfs.txt')
+        outfile = os.path.expanduser('~/temp/Ne10000.txt')
 
     sfs_df.to_csv(outfile)
 
@@ -181,16 +222,16 @@ def simulate_and_plot_sfs(max_count, num_replicates, num_chroms=1, rho=1e-8):
                 num_chroms=num_chroms,
                 rho=rho,
                 )
-        sfs_replicates = pd.DataFrame([get_sfs(ts) for ts in replicates])
-        dfs.append(sfs_replicates)
+        sfs_replicates = [get_sfs(ts, model) for ts in replicates]
+        dfs.extend(sfs_replicates)
 
-    sfs_df = pd.concat(dfs, keys=models)
+    sfs_df = pd.concat(dfs)
     sfs_df = sfs_df.fillna(0)
 
     write_sfs(sfs_df, outfile=None)
-    plot_singletons_doubletons(sfs_df)
+    # plot_singletons_doubletons(sfs_df)
 
 
 if __name__ == "__main__":
-    simulate_and_plot_sfs(max_count=2, num_replicates=2, num_chroms=1, rho=1e-8)
+    simulate_and_plot_sfs(max_count=2, num_replicates=100, num_chroms=1, rho=1e-8)
 
