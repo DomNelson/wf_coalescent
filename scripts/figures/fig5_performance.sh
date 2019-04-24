7a5699e879c571c4fef428ef23130982e0b8265e
DomNelson <nelson.dominic@gmail.com>
2019-04-24 18:33:10 -0400

    First commit

---
+++ scripts/figures/fig5_performance.sh
@@ -0,0 +1,5 @@
+## Main text
+python ../qsub/combine_performance_results.py ../../results/performance/Ne_10000_sample_size_1000/ "*Ne_10000*_1000_"
+
+## Supplement
+# python ../qsub/combine_performance_results.py ../../results/performance/Ne_500_sample_size_500/ "*Ne_500*_500_"
