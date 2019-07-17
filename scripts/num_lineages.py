1940a86eb5cd62fc145e054a3a34cb296a6322ef
DomNelson <nelson.dominic@gmail.com>
2019-07-17 16:39:53 -0400

    Update chrom lengths and finish slurm job submission

--- scripts/num_lineages.py
+++ scripts/num_lineages.py
@@ -4,6 +4,7 @@ import subprocess
 import argparse
 from datetime import datetime
 
+# Kind of a dirty hack but it works for now
 msprime_dir = os.path.expanduser('../../msprime')
 sys.path.insert(1, msprime_dir)
 sys.path.insert(1, msprime_dir + '/lib/subprojects/git-submodules/tskit/python')
@@ -27,46 +28,61 @@ def get_positions_rates(chrom_lengths, rho):
     return positions, rates
 
 
-def srun_job(args):
-    out_dir = os.path.expanduser(args.out_dir)
+def run_sbatch_job(args):
+    job_script = 'job_script.sh'
+    out_dir = args.out_dir
     timestamp = datetime.strftime(datetime.now(), '%Y-%m-%d_%H-%M-%S')
 
-    slurm_cmd = 'srun'
-    job_cmd = 'python num_lineages.py'
-    out_file = os.path.join(out_dir, timestamp)
+    header = "#!/bin/bash\n#SBATCH --mem={}mb\n#SBATCH --time={}\n\n".format(
+            args.mem, args.time)
+
+    cmd = 'python num_lineages.py'
+    out_file = os.path.join(out_dir, 'num_lineages')
     for arg, value in args.__dict__.items():
-        if arg in ['out_dir']:
+        if value is None:
             continue
 
-        if arg in ['srun', 'mem', 'time']:
-            slurm_cmd += ' --' + str(arg) + ' ' + str(value)
-        else:
+        if arg not in ['srun', 'out_dir', 'mem', 'time']:
             out_file += '_' + str(arg) + str(value)
+            cmd += ' --' + str(arg) + ' ' + str(value)
 
-        cmd += ' --' + str(arg) + ' ' + str(value)
+    out_file += '_' + timestamp + '.txt'
+    cmd += ' | python mask_gens.py > ' + out_file
 
-    ## TODO: Need to submit to srun with proper args, which haven't been
-    ## included yet
-    out_file += '.gz'
-    cmd = slurm_cmd + ' ' + job_cmd
-    cmd += ' | gzip > ' + out_file
-    cmd += ' &'
+    with open(job_script, 'w') as f:
+        f.write(header)
+        f.write(cmd)
+        f.write('\n')
 
-    print(cmd)
-    sys.exit()
+    print("Submitting:")
+    print('\n', cmd, '\n')
+    subprocess.run('sbatch job_script.sh', shell=True)
 
 
 def simulate_num_lineages(args):
     Ne = args.Ne
     model = args.model
     sample_size = args.sample_size
+    growth_rate = args.growth_rate
     rho = 1e-8
 
-    chrom_lengths = [247249719, 242951149, 199501827, 191273063, 180857866,
-            170899992, 158821424, 146274826, 140273252, 135374737, 134452384,
-            132349534, 114142980, 106368585, 100338915, 88827254, 78774742,
-            76117153, 63811651, 62435964, 46944323, 49691432]
+    # chrom_lengths = [247249719, 242951149, 199501827, 191273063, 180857866,
+    #         170899992, 158821424, 146274826, 140273252, 135374737, 134452384,
+    #         132349534, 114142980, 106368585, 100338915, 88827254, 78774742,
+    #         76117153, 63811651, 62435964, 46944323, 49691432]
+    chrom_lengths_morgans = [2.77693825, 2.633496065, 2.24483368, 
+		2.12778391, 2.03765845, 1.929517394, 
+		1.867959329, 1.701765192, 1.68073935, 
+		1.789473882, 1.594854258, 1.72777271, 
+		1.26940475, 1.16331251, 1.2554709, 
+		1.348911043, 1.29292106, 1.18978483, 
+		1.077960694, 1.079243479, 0.61526812, 
+		0.72706815]
+
+    chrom_lengths = [l * 1e8 for l in chrom_lengths_morgans]
     chrom_lengths = chrom_lengths[:args.nchroms]
+    print(sum(chrom_lengths))
+
     # chrom_lengths = [1e8] * 10
     # print(sum(chrom_lengths) / 1e6)
     # sys.exit()
@@ -78,18 +94,38 @@ def simulate_num_lineages(args):
             positions, rates, num_loci=num_loci
     )
 
+    demographic_events = None
+    if args.min_pop_size is not None:
+        time_to_min_pop_size = (np.log(Ne) - np.log(args.min_pop_size)) / growth_rate
+        if time_to_pop_size_100 > 0:
+            demographic_events = [
+                    msprime.PopulationParametersChange(
+                        time=time_to_pop_size_100,
+                        growth_rate=0
+                        )
+                    ]
+
+    population_configurations = [
+            msprime.PopulationConfiguration(
+                sample_size=sample_size,
+                initial_size=Ne,
+                growth_rate=growth_rate
+                )
+            ]
+
     seed = np.random.randint(1e9)
-    ts = msprime.simulate(Ne=Ne,
+    ts = msprime.simulate(
+            population_configurations=population_configurations,
+            demographic_events=demographic_events,
             recombination_map=recombination_map,
-            sample_size=sample_size,
             model=model,
             random_seed=seed
-            )
+        )
 
 
 def main(args):
-    if args.srun is not None:
-        srun_job(args)
+    if args.srun is True:
+        run_sbatch_job(args)
     else:
         simulate_num_lineages(args)
 
@@ -97,7 +133,9 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--Ne', default=50, type=int)
+    parser.add_argument('--min_pop_size', type=int)
     parser.add_argument('--sample_size', default=50, type=int)
+    parser.add_argument('--growth_rate', default=0.005, type=float)
     parser.add_argument('--model', default='Hudson')
     parser.add_argument('--nchroms', default=22, type=int)
     parser.add_argument('--srun', action="store_true")
@@ -107,6 +145,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    if args.min_pop_size is not None:
+        assert (args.min_pop_size <= args.Ne)
+
     main(args)
-
-
